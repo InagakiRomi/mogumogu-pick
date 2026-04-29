6e1dcc9aea98fc695ba74dbdd2e4b80daf63d696
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2026-04-29 16:42:10 +0800

    feat: 建置 springboot 專案

---
+++ backend/src/main/java/com/romi/mogumogu/MogumoguApplication.java
@@ -0,0 +1,13 @@
+package com.romi.mogumogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogumoguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogumoguApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/romi/mogumogu/MogumoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romi.mogumogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogumoguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
